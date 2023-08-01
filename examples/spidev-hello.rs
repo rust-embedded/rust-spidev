9595bab29490b54f8349f00ab0ddfbe8f7b1ccc2
Diego Barrios Romero <eldruin@gmail.com>
2023-08-01 18:34:52 +0200

    Fix warning about unused result

--- examples/spidev-hello.rs
+++ examples/spidev-hello.rs
@@ -4,9 +4,9 @@ use std::io::prelude::*;
 
 fn main() {
     let mut spidev = Spidev::open("/dev/spidev0.0").unwrap();
-    spidev.write(&[0xAA, 0x00, 0x01, 0x02, 0x04]).unwrap();
+    let wrote = spidev.write(&[0xAA, 0x00, 0x01, 0x02, 0x04]).unwrap();
 
     let mut buf: [u8; 10] = [0; 10];
-    spidev.read(&mut buf).unwrap(); // read 10
-    println!("{:?}", buf);
+    let read = spidev.read(&mut buf).unwrap(); // read 10
+    println!("Wrote: {}, Read: {}, Data: {:?}", wrote, read, buf);
 }
